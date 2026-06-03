b1b158f9ec5660502561de1e8d40bbb98104198f
Ganesh <ganeshgarje716@gmail.com>
2026-06-03 20:48:12 +0530

    Add Some Variables and Annotations in Product Class

--- SpringBoot-Product/src/main/java/com/ganesh/entity/Product.java
+++ SpringBoot-Product/src/main/java/com/ganesh/entity/Product.java
@@ -1,5 +1,51 @@
 package com.ganesh.entity;
 
-public interface Product {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Product {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true)
+	private String name;
+	
+	private String type;
+	
+	private Integer qty;
+	
+	private Double price;
+	
+	private Boolean active;
+
+	
+	
+	
+	
+	public Product(Integer id, String name, String type, Integer qty, Double price, Boolean active) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.qty = qty;
+		this.price = price;
+		this.active = active;
+	}
+	
+	
+	
+	
+	
 
 }
