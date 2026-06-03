753d876a89ca528937cf4dc86b022790e7dac339
Ganesh <ganeshgarje716@gmail.com>
2026-06-03 20:52:36 +0530

    implements CommandLineRunner and Override run() Method

--- SpringBoot-Product/src/main/java/com/ganesh/SpringBootProductApplication.java
+++ SpringBoot-Product/src/main/java/com/ganesh/SpringBootProductApplication.java
@@ -1,13 +1,20 @@
 package com.ganesh;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProductApplication {
+public class SpringBootProductApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProductApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
