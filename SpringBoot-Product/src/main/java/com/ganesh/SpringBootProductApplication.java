270946b65eaf586206caabc15f51090a49fa4cec
Ganesh <ganeshgarje716@gmail.com>
2026-06-03 20:57:52 +0530

    Create ProductRepository interface Variable and Write on Head @Autowierd Annotation

--- SpringBoot-Product/src/main/java/com/ganesh/SpringBootProductApplication.java
+++ SpringBoot-Product/src/main/java/com/ganesh/SpringBootProductApplication.java
@@ -1,8 +1,11 @@
 package com.ganesh;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ganesh.repository.ProductRepository;
 
 @SpringBootApplication
 public class SpringBootProductApplication implements CommandLineRunner{
@@ -10,6 +13,12 @@ public class SpringBootProductApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProductApplication.class, args);
 	}
+	
+	
+	@Autowired
+	ProductRepository PR;
+	
+	
 
 	@Override
 	public void run(String... args) throws Exception {
